b6042cf20042d80294b079d5a5df1c69164ede2b
MasakiFukunishi <fmsknn@gmail.com>
2024-02-06 00:09:38 +0900

    add getCreatorCollections

--- hardhat/contracts/ERC721Factory.sol
+++ hardhat/contracts/ERC721Factory.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import './ERC721Collection.sol';
+import "./ERC721Collection.sol";
 
 contract ERC721Factory {
-    // mapping(address => address) public collectionCreators;
-
     event ERC721Created(address indexed creator, address erc721);
 
-    function createERC721Collection(string memory name, string memory symbol) external returns (address erc721) {  
+    mapping(address => address[]) private creatorCollections;
+
+    function createERC721Collection(string memory name, string memory symbol) external returns (address erc721) {
         ERC721Collection erc721Contract = new ERC721Collection(name, symbol);
         erc721 = address(erc721Contract);
-        // collectionCreators[erc721] = msg.sender;
+
+        creatorCollections[msg.sender].push(erc721);
+
         emit ERC721Created(msg.sender, erc721);
+
+        return erc721;
+    }
+
+    function getCreatorCollections(address creator) external view returns (address[] memory) {
+        return creatorCollections[creator];
     }
 }
