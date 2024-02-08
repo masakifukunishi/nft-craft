c3f44c0b2f64ab4e6b7ab267c9f46d2a4b08cda4
fukunishi <fukunishi@hashhub.tokyo>
2024-02-08 09:51:32 +0900

    add Ownable

--- hardhat/contracts/ERC721Collection.sol
+++ hardhat/contracts/ERC721Collection.sol
@@ -3,23 +3,21 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC721Collection is ERC721, ERC721URIStorage {
-    address immutable public factory;
-    uint256 private tokenIdCounter;
-
+contract ERC721Collection is ERC721, ERC721URIStorage, Ownable {
+    uint256 private tokenIdCounter = 0;
     uint256 constant MAX_SUPPLY = 1000;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
-        factory = msg.sender;
+    constructor(string memory _name, string memory _symbol, address creator) ERC721(_name, _symbol) {
+        transferOwnership(creator);
     }
 
-    function safeMint(address to, string memory uri) public {
-        uint256 tokenId = tokenIdCounter;
-        require(tokenId < MAX_SUPPLY, "Max supply reached");
+    function safeMint(address to, string memory uri) public onlyOwner {
+        require(tokenIdCounter < MAX_SUPPLY, "Max supply reached. Cannot mint more.");
+        uint256 tokenId = tokenIdCounter++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        tokenIdCounter += 1;
     }
 
     function tokenURI(uint256 tokenId)
