3e93ebe90219e3abc60c0e1ce268b9ea3efda1fe
MasakiFukunishi <fmsknn@gmail.com>
2024-02-09 23:06:47 +0900

    add redux

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,6 +4,13 @@ export const store = configureStore({
   reducer: {
     wallet: walletReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["wallet/setWallet"],
+        ignoredPaths: ["wallet.signer"],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
