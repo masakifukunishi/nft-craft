c24c8ee4f35648a72f408fe4fe94814bf61db5c7
MasakiFukunishi <fmsknn@gmail.com>
2024-02-12 17:31:40 +0900

    add collection page

--- src/pages/create/erc-721/collection.tsx
+++ src/pages/create/erc-721/collection.tsx
@@ -1,7 +1,25 @@
 import { NextPage } from "next";
+import Header from "../../../components/header";
 
 const createCollection: NextPage = () => {
-  return <div>collection</div>;
+  return (
+    <div>
+      <Header />
+      <div className="flex flex-col items-center mt-2">
+        <form className="w-1/3">
+          <h2 className="text-xl font-semibold">Create new collection</h2>
+          <div className="mt-4">
+            <div className="text-lg font-semibold">Standard</div>
+            <div className="text-lg">ERC-721</div>
+          </div>
+          <div className="mt-4">
+            <div className="text-lg font-semibold">Choose blockchain</div>
+            <div className="text-lg">ERC-721</div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default createCollection;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,7 +119,7 @@ const Home: NextPage = () => {
     console.log("tx", tx);
   };
   return (
-    <div className="min-h-screen px-4">
+    <div className="min-h-screen">
       <Header />
       <div>
         <div className="text-gray-50">ChainId: {chainId}</div>
