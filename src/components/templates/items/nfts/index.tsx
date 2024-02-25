56345a1ae837846b19df6c5eaec02f30a667713e
MasakiFukunishi <fmsknn@gmail.com>
2024-02-25 10:56:16 +0900

    add metadata

--- src/components/templates/items/nfts/index.tsx
+++ src/components/templates/items/nfts/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import { useAccount } from "wagmi";
 
 import Tab from "@/components/organisms/items/tab";
@@ -29,12 +30,21 @@ const itemsNFTs = () => {
       <Tab />
       <div className="text-lg">NFTs</div>
       <div>
-        {nfts.map((nft: EvmNft) => (
-          <div key={nft.tokenHash} className="flex items-center">
-            <div className="bg-green-300 w-10 h-10 rounded-full" />
-            <div className="text-lg">{nft.name}</div>
-          </div>
-        ))}
+        {nfts.map((nft: EvmNft) => {
+          const metadata = typeof nft.metadata === "string" ? JSON.parse(nft.metadata) : nft.metadata;
+          const image = metadata?.image ? metadata.image.replace("ipfs://", "https://ipfs.io/ipfs/") : "";
+          console.log("image", image);
+
+          return (
+            <div key={nft.tokenHash} className="">
+              <div className="bg-green-300 w-10 h-10 rounded-full" />
+
+              <div className="text-lg">{metadata.name}</div>
+              <div className="text-lg">{metadata.description}</div>
+              {image && <Image src={image} alt="NFT Image" width={200} height={200} />}
+            </div>
+          );
+        })}
       </div>
     </>
   );
