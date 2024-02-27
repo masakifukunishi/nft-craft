1d146d7b0e3b711beace39ac57ba55967c387ade
MasakiFukunishi <fmsknn@gmail.com>
2024-02-27 23:00:30 +0900

    fix nft and collection name

--- src/components/organisms/items/nfts/Card.tsx
+++ src/components/organisms/items/nfts/Card.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 
+import shortenString from "@/utills/shortenString";
+
 type Props = {
-  collectionName: string;
+  collectionName: string | undefined;
   name: string | undefined;
   image: string | null;
 };
 
 const Card = ({ collectionName, name, image }: Props) => {
-  const effectiveName = name || "Unnamed...";
+  const effectiveCollectionName = collectionName ? shortenString(collectionName, 22) : "Unnamed...";
+  const effectiveName = name ? shortenString(name, 22) : "Unnamed...";
   return (
     <div className="md:w-64 md:h-80 lg:w-72 lg:h-96 w-48 h-64 bg-base-black-light rounded-lg relative overflow-hidden">
       {image && (
@@ -16,7 +19,7 @@ const Card = ({ collectionName, name, image }: Props) => {
         </div>
       )}
       <div className="p-5 absolute md:bottom-1 lg:bottom-2 bottom-0">
-        <h4 className="mb-1 text-gray-400 md:text-base text-sm">{collectionName}</h4>
+        <h4 className="mb-1 text-gray-400 md:text-base text-sm">{effectiveCollectionName}</h4>
         <h5 className="mb-1 md:text-lg text-base font-semibold tracking-tight text-white">{effectiveName}</h5>
       </div>
     </div>
