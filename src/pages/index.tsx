df62149af43cf95742d7689af56b614caf9d4abc
MasakiFukunishi <fmsknn@gmail.com>
2024-02-28 21:50:30 +0900

    fix min height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from "@/components/templates/Layout";
 const Home: NextPage = () => {
   return (
     <Layout title="NFT MINTING, Mint & Display NFTs" isUseDefaultTitle={false}>
-      <div className="min-h-screen">Create your own NFT</div>
+      <div className="min-h-[calc(100vh-4.5rem)]">Create your own NFT</div>
     </Layout>
   );
 };
