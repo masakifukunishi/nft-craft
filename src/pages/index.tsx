05d486bd662bdbcb6c28ab866f031b26000427f1
MasakiFukunishi <fmsknn@gmail.com>
2024-02-29 09:10:37 +0900

    add top style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,9 @@ import Layout from "@/components/templates/Layout";
 const Home: NextPage = () => {
   return (
     <Layout title="NFT MINTING, Mint & Display NFTs" isUseDefaultTitle={false}>
-      <div className="min-h-[calc(100vh-4.5rem)] bg-indigo-500 rounded-xl">
-        <div className="flex justify-center items-center pt-8">
-          <div className="flex justify-center items-center bg-base-black w-96 h-96 rounded-lg text-4xl">Web3</div>
-          <div className="text-2xl font-semibold">NFT MINTING</div>
+      <div className="min-h-[calc(100vh-4.5rem)] rounded-xl">
+        <div className="flex justify-center items-center bg-yellow-300 text-base-black w-96 h-96 rounded-lg">
+          <div className="text-4xl font-bold">WEB3</div>
         </div>
       </div>
     </Layout>
