921057e69b89baf3084ca9b5bcf1ee4ce25d6255
MasakiFukunishi <fmsknn@gmail.com>
2024-03-02 15:22:39 +0900

    fix top page design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <Layout title="NFT minting, Mint & Display NFTs" isUseDefaultTitle={false}>
       <div className="min-h-[calc(100vh-4.5rem)] ">
         <div className="min-h-[calc(100vh-9.5rem)] bg-yellow-300 rounded-xl flex justify-center items-center">
-          <div className="flex justify-center items-center flex-col md:flex-row gap-10">
+          <div className="flex justify-center items-center flex-col md:flex-row gap-10 my-5">
             <div className="flex justify-center items-center bg-base-black text-base-black xl:w-118 xl:h-118 lg:w-96 lg:h-96 md:w-72 md:h-72 w-64 h-64 rounded-lg">
               <div className="text-4xl text-yellow-300 font-bold text-center">
                 <h3 className="mb-3">WEB3</h3>
