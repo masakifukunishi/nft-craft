3a0a4a66a4c9a85db2ff3d7f910926efc9b80f63
MasakiFukunishi <fmsknn@gmail.com>
2024-03-03 19:42:12 +0900

    fix config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -4,14 +4,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const privateKey0: string = process.env.PRIVATE_KEY ?? "";
-if (privateKey0 === "") {
-  throw new Error("No value set for environement variable PRIVATE_KEY");
-}
-
 const sepoliaUrl: string = process.env.SEPOLIA_URL ?? "";
-if (sepoliaUrl === "") {
-  throw new Error("No value set for environement variable SEPOLIA_URL");
-}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
