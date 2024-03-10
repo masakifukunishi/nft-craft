ff6a27b2b9dacbda4ca33faaafb859ebcd8bb269
MasakiFukunishi <fmsknn@gmail.com>
2024-03-10 16:06:01 +0900

    fix env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,7 +16,7 @@ export const authOptions: AuthOptions = {
       async authorize(credentials, req) {
         try {
           const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"));
-          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL!);
+          const nextAuthUrl = new URL(process.env.NEXT_PUBLIC_VERCEL_URL!);
 
           const result = await siwe.verify({
             signature: credentials?.signature || "",
